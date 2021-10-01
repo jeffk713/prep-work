c5eb0eb5063a93a41dd608f46d7bb30e159daaa1
jeff <jeffk713@gmail.com>
2021-09-30 21:36:03 -0700

    condiitonal prep module commit

---
+++ lighthouse-js-fundamentals/tinkering.js
@@ -0,0 +1,11 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(2, 4);
+
+let greeting = 'Hello ';
+const name = 'Justine';
+greeting += name;
+
+console.log(greeting);
--- prep.js
+++ prep.js
@@ -1,1 +1,45 @@
-const a = 5;
+const a = 'zbc';
+const b = 0;
+const c = 3;
+// console.log(a > b);
+
+/*
+console.log(true && 10); //true
+console.log(false && 10); //false
+
+console.log(a && 20); //20
+console.log(b && 30); //0
+console.log(c && 40); //40
+
+console.log(!!c === true); //true
+
+console.log(true || false); //true
+console.log(false || true); //true
+console.log(false == 0); //true
+console.log(true == 1);
+*/
+
+const salary = 100;
+let car = '';
+
+switch (salary) {
+  case 1000:
+    car = 'shit';
+    console.log('shit');
+    break;
+  case 3000:
+    car = 'okay';
+    console.log('okay');
+    break;
+
+  case 5000:
+    car = 'good';
+    console.log('good');
+    break;
+  default:
+    car = 'car';
+    console.log('car');
+    break;
+}
+
+console.log(car);
